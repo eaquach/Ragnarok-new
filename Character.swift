3158d0f052d1331f331e10f8290314f51559a697
Elizabeth <91192031+eaquach@users.noreply.github.com>
2022-03-30 15:38:10 +0200

    update
    add class Character

---
+++ Character.swift
@@ -0,0 +1,83 @@
+
+import Foundation
+
+// Character class, parent class of all the characters
+
+class Character {
+    
+    static var names = [String]()
+    
+    //    settings of all characters
+    var name:String
+    var lifePoints = 100
+    var weapon : Weapon
+    let maxCharacterTeam = 3
+    init(weapon: Weapon, name: String) {
+        self.weapon = weapon
+        self.name = name
+    }
+    
+    
+    
+    func attack(opponent : Character) {
+        print("attack from \(name)","damage points with \(weapon.name)")
+        opponent.lifePoints -= (weapon.damagePoints)
+        print("\(opponent.name) looses \(weapon.damagePoints) pts")
+        
+    }
+    
+    
+    func cure(target : Character){
+        let curePoints = 10
+        target.lifePoints += curePoints
+        print("\(target.name) recovered \(curePoints)")
+    }
+       
+    
+    
+    func characterIsAlive() -> Bool {
+        if lifePoints > 80 {
+            print ("\(name) is alive")
+            return true
+        } else {
+            print("Your \(name) is dead, choose another character to fight")
+            return false
+        }
+        
+    }
+    
+    func displayLifepoints() {
+        print("\(name) has \(lifePoints) life points")
+              }
+    
+
+    
+    
+    func chestBonus(chest: Chest) {
+        weapon = chest
+    }
+    
+  
+    
+            
+            
+   
+                            
+
+
+}
+
+extension Character: Equatable {
+    static func == (lhs: Character, rhs: Character) -> Bool {
+        lhs.name == rhs.name && lhs.weapon == rhs.weapon
+    }
+    
+  
+    
+}
+
+
+
+    
+
+
